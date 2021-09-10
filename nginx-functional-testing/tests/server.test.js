efc61b8a159afda585f664f223c3cd7ca126f2ee
Abhishek Gupta <abhishek2411@outlook.com>
2021-09-10 15:17:21 +0100

    Add test

--- nginx-functional-testing/tests/server.test.js
+++ nginx-functional-testing/tests/server.test.js
@@ -2,14 +2,14 @@ const request = require("supertest");
 const server = request("http://mywebsite.com");
 
 describe("Test localhost server", () => {
-    test("/mit returns 200", () => {
+    test("/washington returns 200", () => {
         return server.get("/washington").then(res => {
             expect(res.status).toBe(200);
             expect(res.body.toString()).toBe("Washington response body");
         });
     });
 
-    test("/nyu returns 200", () => {
+    test("/example returns 200", () => {
         return server.get("/example").then(res => {
             expect(res.status).toBe(200);
             expect(res.body.toString()).toBe("Example response body");
@@ -17,9 +17,9 @@ describe("Test localhost server", () => {
     });
 
     test.each`
-        cookie_input          |       cookie_output
-        ${'site=washington;'}        |       ${'site=washington;'}
-        ${'site=example;'}        |       ${'site=example;'}
+        cookie_input                |       cookie_output
+        ${'site=washington;'}       |       ${'site=washington;'}
+        ${'site=example;'}          |       ${'site=example;'}
     `('$cookie_input, $cookie_output', async ({cookie_input, cookie_output}) => {
         return server.get("/site").set("Cookie", [cookie_input]).then(res => {
             expect(res.status).toBe(200);
